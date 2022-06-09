c149c3fab7f8aa6f7c032bd499752c26af8cc599
macszim <malfonso@udesa.edu.ar>
2022-06-09 15:27:34 -0300

    trabajando en js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,49 @@
-let buscador = document.querySelector('busc')
-let formulario = document.getElementById('formulario')
-console.log(formulario);
-buscador.addEventListener('submit', function (b) {
-    b.preventDefault();
-    if (formulario.value === '') {
-        alert('EL CAMPO NO PUEDE ESTAR VACIO')
-    } else if (formulario.value.length < 3) {
-        alert('EL CAMPO REQUIERE DE UN MINIMO DE 3 DIGITOS')
-    } else {
-        buscador.submit();
-    }
-})
+//let buscador = document.querySelector('form')
+//let formulario = document.getElementById('formulario')
+////let errorCasilla = document.querySelector(".error-casilla")
+ //console.log(formulario);
+//buscador.addEventListener('submit', function (e) {
+  //  e.preventDefault();
+    //if (formulario.value === '') {
+      // errorCasilla.innerText = 'no puede estar vacio'
+        
+    //} else if (formulario.value.length < 3) {
+      //  alert('se necesitan mas letras')
+    //} else {
+ //       buscador.submit();
+   // }
+//})
 
-fetch("https://api.allorigins.win/get?url=https://api.deezer.com/chart/")
+let buscador="https://api.allorigins.win/raw?url=https://api.deezer.com/chart/")
+fetch(buscador)
 .then(function (response) {
 return response.json();
 })
+.then(function(data){
+    console.log(data);
+})
+let info = data.data
+
+//capturar el contenedor de la lista
+
+let section = document.querySelector(".listas");
+let articles = "";
+ //Los elementos están en un array y para obtenerlos hay recorrerlo.
+ for(let i=0; i<info.length; i++){
+    //construir un elemento de lista
+    articles += `<article class="lista">
+                    <p> class="caciones">Nombre: ${data.data[i].title}</p>
+                    <img class="image" src="${data.data[i].images.original.url}" alt="">
+                    <a href="detail-artist.html?id=${data.data[i].id}">ir a detalle </a>
+                </article>`
+}
+console.log(articles);
+
+section.innerHTML += articles
+
+
+})
+.catch(function(error){
+console.log(error);
+})
+
