caa4b0d0fdb27e88631c0a8a50524f3a1d4275cc
macszim <malfonso@udesa.edu.ar>
2022-06-10 14:07:32 -0300

    trabajando en el javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
-let buscador="https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks"
-fetch(buscador)
+let tracks="https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks"
+fetch(tracks)
     .then(function (response) {
         return response.json();
     })
@@ -11,12 +11,13 @@ fetch(buscador)
         let section = document.querySelector(".canciones");
         let articles = "";
 
-        for(let i=0; i<info.length; i++){
+        for(let i=0; i<5; i++){
             //construir un elemento de lista
             articles += `<article>
-                            <h1 class="caciones">Nombre: ${info[i].title}</h1>
-                            <img class="image" src="${info[i].artist.picture_big}" alt="">
-                            <a class="link" href="detail-artist.html?id=${info[i].id}">ir a detalle </a>
+                            <img class="imagen" src="${info[i].artist.picture_big}" alt="">
+                            <h2 class="nombres">Nombre:<a href="./detail-track.html?id=${infoArtist[i].id}">${info[i].title}</a></h2>
+                            <p class="nombresgrupo">Artista:<a href="./detail-track.html?id=${infoArtist[i].id}">${info[i].artist.name}</a></p>
+                            <p class="nombresgrupo">Lanzamiento:${info[i].release_date}</p>
                         </article>`
                     }
                     
@@ -25,11 +26,49 @@ fetch(buscador)
             
             
             //Los elementos están en un array y para obtenerlos hay recorrerlo.
-            console.log('articles');
+            console.log(articles);
             section.innerHTML += articles
     })
 
     .catch(function(error){
         console.log(error);
     })
+
+
+    let Artista="https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists"
+    fetch(Artista)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+    
+            let infoArtist = data.data
+            let section = document.querySelector(".artistas");
+            let articles = "";
+    
+            for(let i=0; i<5; i++){
+                //construir un elemento de lista
+                articles += `<article>
+                                <img class="imagen" src="${infoArtist[i].picture_big}" alt="">
+                                <h2 class="nombres">Artista:<a href="./detail-artist.html?id=${infoArtist[i].id}">${infoArtist[i].name}</a></h2>
+                                <p class="nombresgrupo">Top:${infoArtist[i].position}</p>
+                                <p class="nombres">Tracklist:${infoArtist[i].tracklist}</p>
+                    
+                            </article>`
+                        }
+                        
+                //capturar el contenedor de la lista
+                
+                
+                
+                //Los elementos están en un array y para obtenerlos hay recorrerlo.
+                console.log('articles');
+                section.innerHTML += articles
+        })
+    
+        .catch(function(error){
+            console.log(error);
+        })
+    
 
