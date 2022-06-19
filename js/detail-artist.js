cfb2df70241b1592878056841abab5b8d58d077c
macszim <malfonso@udesa.edu.ar>
2022-06-19 20:13:18 -0300

    trabajando en equipo detail artist

--- js/detail-artist.js
+++ js/detail-artist.js
@@ -1,43 +1,55 @@
-let DetalleArtist = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27"
-fetch(DetalleArtist)
+let queryString = location.search;
+let datos = new URLSearchParams(queryString);
+let objartist = datos.get('id');
+console.log(queryString);
+console.log(datos);
+console.log(objartist);
+
+let detailArtist = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/`
+fetch(detailArtist + objartist)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
 
-      
         let section = document.querySelector(".detalleArtista");
-        let articles = "";
 
-        for (let i = 0; i <1; i++) {
+
+        for (let i = 0; i < 1; i++) {
             //construir un elemento de lista
-            articles += `<article>
-                            <h1 class="nombres">Nombre: ${data.name}</h1>
-                        <img class="dtimage" src="${data.cover_big}" alt="">
-                        </article>
-                                <h2><a class="nombres" href="detail-artist.html">${data.artist.name}</a></h2>
-                                <p><a class="nombres" href="detail-artist.html"> Genero:${data.name}</a></p>
-                                <p>Lanzamiento del disco: ${data.release_date}</p>
-                                <ol>
-                                    <li> <a href="playlist.html" type="submit">${data.tracks.title}<i class="fa-solid fa-heart-circle-plus"></i></button> </li>
-                                    <li> <a href="playlist.html" type="submit">${data.tracks.title}<i class="fa-solid fa-heart-circle-plus"></i></button> </li>
-                                    <li> <a href="playlist.html" type="submit">${data.tracks.title}<i class="fa-solid fa-heart-circle-plus"></i></button> </li>
-                                    <li> <a href="playlist.html" type="submit">${data.tracks.title}<i class="fa-solid fa-heart-circle-plus"></i></button> </li>
-                                    <li> <a href="playlist.html" type="submit">${data.tracks.title}<i class="fa-solid fa-heart-circle-plus"></i></button> </li> 
-                    </ol >`
-                    
-                                }
-
-        //capturar el contenedor de la lista
-
-
-
-        //Los elementos están en un array y para obtenerlos hay recorrerlo.
-        console.log(articles);
-        section.innerHTML += articles
+            section.innerHTML += `<article>
+            <h1 class="titulos">Tu Artista: ${data.name} </h1>
+            <article class="contenedortrack">
+        <img class="itemimg" src="${data.picture_big}" alt="'${data.id}'">
+        <h2 class="nombres"><a class="nombrestrack" href="detail-artist.html?id=${data.name}">Artista:${data.name}</a></h2><p class="nombres">Fans: ${data.nb_fan}</p>  
+        </article>
+        </article> `
+        }
     })
 
-    .catch(function (error) {
-        console.log(error);
+let dataAlbum = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/`
+fetch(dataAlbum + objartist + '/albums')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        let albums = data.data
+        let listadoAlbum = document.querySelector(".listadoAlbum");
+
+        for (let i = 0; i < 6; i++) {
+            listadoAlbum.innerHTML = `
+    <article class="contenedordetailartist"> <a href="detail-album.html?id=${albums[1].id}"><li class="detalleartist">${albums[1].title }<br><img class="imgartist" src="${albums[1].cover_big}" alt="${albums[1].title}"></li></a>
+    <a href="detail-album.html?id=${albums[2].id}"><li class="detalleartist">${albums[2].title }<br><img class="imgartist" src="${albums[2].cover_big}" alt="${albums[2].title}"></li></a>
+    <a href="detail-album.html?id=${albums[3].id}"><li class="detalleartist">${albums[3].title }<br><img class="imgartist" src="${albums[3].cover_big}" alt="${albums[3].title}"></li></a>
+    <a href="detail-album.html?id=${albums[4].id}"><li class="detalleartist">${albums[4].title }<br><img class="imgartist" src="${albums[4].cover_big}" alt="${albums[4].title}"></li></a>
+    <a href="detail-album.html?id=${albums[5].id}"><li class="detalleartist">${albums[5].title }<br><img class="imgartist" src="${albums[5].cover_big}" alt="${albums[5].title}"></li></a>
+    <a href="detail-album.html?id=${albums[6].id}"><li class="detalleartist">${albums[6].title }<br><img class="imgartist" src="${albums[6].cover_big}" alt="${albums[6].title}"></li></a>
+          </article> `
+
+
+        }
+
     })
